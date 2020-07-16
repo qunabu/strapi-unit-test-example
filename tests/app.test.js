6028c64a131688fd10314c59deb7c5abc00bebd9
Kazdan1994 <arthurjacquemin2@gmail.com>
2020-07-16 16:25:07 +0200

    fix(tests): increase timeout for jest

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const { setupStrapi } = require("./helpers/strapi");
 
+jest.setTimeout(10000);
+
 /** this code is called once before any test is called */
 beforeAll(async () => {
   await setupStrapi(); // singleton so it can be called many times
