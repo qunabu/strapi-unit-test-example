1def131fa81ad3884f2cf790971133958858deec
Mateusz <mateusz@qunabu.com>
2020-10-15 19:54:44 +0200

    More examples for medium article

--- tests/user/index.js
+++ tests/user/index.js
@@ -31,7 +31,7 @@ describe("Default User methods", () => {
         const verified = await strapi.plugins[
           "users-permissions"
         ].services.jwt.verify(data.body.jwt);
-        expect(data.body.jwt == jwt || verified).not.toBe(true); // jwt does have a random seed, each issue can be different
+        expect(data.body.jwt == jwt || !!verified).toBe(true); // jwt does have a random seed, each issue can be different
       });
 
     done();
